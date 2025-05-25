d18d31141751eac4892f35e5e18448b65fc34eb8
Aayushdubey05 <dubeyaayush333@gmail.com>
2025-05-25 15:33:25 +0530

    initial commits

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService, Home } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, Home],
+  providers: [AppService, Home],
 })
 export class AppModule {}
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -6,3 +6,14 @@ export class AppService {
     return 'Hello World!';
   }
 }
+
+@Injectable()
+export class Home {
+  public getHome(): object{
+    return {
+      title: 'Home',
+      description: 'Welcome to the home page!',
+      content: 'This is the content of the home page.',
+    };
+  }
+}  
